e456a64590f4a0b9676e6f99f40bec3004b12f0b
Jeremiahpaul7 <244x4sxs4s@privaterelay.appleid.com>
2026-05-21 13:19:27 +0530

    Refactor SQL queries and update numbering
    Reorganize and renumber SQL queries for clarity and consistency.

--- sql_query_p2.sql
+++ sql_query_p2.sql
@@ -36,8 +36,7 @@ Q.4 v1 Calculate the total VAT collected for each payment method.
 Q.5 Find the total sales amount and average customer rating for each branch.
 Q.6 Calculate the total sales amount for each city and gender combination.
 Q.7 Find the average quantity of products sold for each product line to female customers.
-Q.8 Count the number of sales transactions for members in each branch.
-Q.9 Find the total sales amount for each day. (Return day name and their total sales order DESC by amt)
+Q.8 Find the total sales amount for each day. (Return day name and their total sales order DESC by amt)
 
 
 
@@ -45,17 +44,17 @@ Q.9 Find the total sales amount for each day. (Return day name and their total s
 -- ---------------------------------------------
 -- Business Problems :: Advanced Level
 -- ---------------------------------------------
-Q.10 Calculate the total sales amount for each hour of the day
-Q.11 Find the total sales amount for each month. (return month name and their sales)
-Q.12 Calculate the total sales amount for each branch where the average customer rating is greater than 8.
-Q.13 Find the total VAT collected for each product line where the total sales amount is more than 500.
-Q.14 Calculate the average sales amount for each gender in each branch.
-Q.15 Count the number of sales transactions for each day of the week.
-Q.16 Find the total sales amount for each city and customer type combination where the number of sales transactions is greater than 50.
-Q.17 Calculate the average unit price for each product line and payment method combination.
-Q.18 Find the total sales amount for each branch and hour of the day combination.
-Q.19 Calculate the total sales amount and average customer rating for each product line where the total sales amount is greater than 1000.
-Q.20 Calculate the total sales amount for morning (6 AM to 12 PM), afternoon (12 PM to 6 PM), and evening (6 PM to 12 AM) periods using the time condition.
+Q.9 Calculate the total sales amount for each hour of the day
+Q.10 Find the total sales amount for each month. (return month name and their sales)
+Q.11 Calculate the total sales amount for each branch where the average customer rating is greater than 8.
+Q.12 Find the total VAT collected for each product line where the total sales amount is more than 500.
+Q.13 Calculate the average sales amount for each gender in each branch.
+Q.14 Count the number of sales transactions for each day of the week.
+Q.15 Find the total sales amount for each city and customer type combination where the number of sales transactions is greater than 50.
+Q.16 Calculate the average unit price for each product line and payment method combination.
+Q.17 Find the total sales amount for each branch and hour of the day combination.
+Q.18 Calculate the total sales amount and average customer rating for each product line where the total sales amount is greater than 1000.
+Q.19 Calculate the total sales amount for morning (6 AM to 12 PM), afternoon (12 PM to 6 PM), and evening (6 PM to 12 AM) periods using the time condition.
 
 */
 
@@ -100,6 +99,12 @@ SELECT
 FROM walmart_sales
 GROUP BY customer_type
 
+--4 Find the total quantity of products sold for each product line.
+
+select  product_line, sum(quantity) from walmart_sales 
+group by product_line
+
+
 -- MEDIUM DIFFICULTY
 -- Q.5 Find the total sales amount and average customer rating for each branch.
 
@@ -132,7 +137,7 @@ WHERE gender = 'Male'
 GROUP BY product_line
 
 
--- Q.9 Find the total sales amount for each day. (Return day name and their total sales order DESC by amt)
+-- Q.8 Find the total sales amount for each day. (Return day name and their total sales order DESC by amt)
 
 SELECT
     TO_CHAR(date, 'Day') as day_name,
@@ -144,7 +149,7 @@ ORDER BY total_sale DESC
 
 
 -- Advanced 
--- Q.10 Calculate the total sales amount for each hour of the day
+-- Q.9 Calculate the total sales amount for each hour of the day
 
 
 SELECT 
@@ -156,7 +161,7 @@ ORDER BY 2
 
 
 
--- Q.11 Find the total sales amount for each month. (return month name and their sales)
+-- Q.10 Find the total sales amount for each month. (return month name and their sales)
 
 
 
@@ -169,42 +174,94 @@ GROUP BY month_name
 ORDER BY total_sale DESC
 
 
--- Q.12 Calculate the total sales amount for each branch where the average customer rating is greater than 7.
+-- Q.11 Calculate the total sales amount for each branch where the average customer rating is greater than 7.
 
 SELECT 
     branch,
     SUM(total) as total_sale, --- having
     AVG(rating) as avg_rating -- having
 FROM walmart_sales
-WHERE branch <> 'E'
 GROUP BY 1
 HAVING AVG(rating) > 7
 
--- WHERE and HAVING
+-- 12  Find the total VAT collected for each product line where the total sales amount is more than 500.
 
+select product_line, 
+       sum(vat) as vat,
+	   sum(total) as total_sales
+from walmart_sales 
+group by product_line 
+having sum(total) >500
 
+--13 Calculate the average sales amount for each gender in each branch.
 
--- Q.20 Calculate the total orders amount for morning (6 AM to 12 PM), afternoon (12 PM to 6 PM), and evening (6 PM to 12 AM) periods using the time condition.
+select branch, gender, avg(total) from walmart_sales 
+group by 1,2
+order by 1 
 
-WITH new_table
-AS
-(
-SELECT *,
-    CASE
-        WHEN EXTRACT(HOUR FROM time) BETWEEN 6 AND 12 THEN 'Morning'
-        WHEN EXTRACT(HOUR FROM time) > 12 AND  EXTRACT(HOUR FROM time) <= 18 THEN 'Afternoon'
-        ELSE 'Evening'
-    END as shift    
-FROM walmart_sales
+--14 Count the number of sales transactions for each day of the week.
+
+select 
+      to_char(date,'day')as day,
+      count(*) as sales_transactions
+from walmart_sales 
+group by day
+order by day
+
+-- 15 Find the total sales amount for each city and customer type combination where the number of sales transactions is greater than 50.
+
+select city, customer_type, sum(total) as total_sales, count(*) as sales_transactions from walmart_sales 
+group by 1,2
+having count(*) > 50
+order by 1
+
+-- 16 Calculate the average unit price for each product line and payment method combination.
+
+select product_line, payment_method, avg(unit_price) from walmart_sales 
+group by 1,2 
+order by 1
+
+-- 17 Find the total sales amount for each branch and hour of the day combination.
+
+select
+      branch,
+      extract(hour from time) as hour_of_the_day,
+	  sum(total)
+from walmart_sales
+group by branch, hour_of_the_day 
+order by 1,2
+
+-- 18 Calculate the total sales amount and average customer rating for each product line where the total sales amount is greater than 50000.
+
+select 
+      product_line,
+	  sum(total) as total_sales,
+	  avg(rating)
+from walmart_sales
+group by 1
+having sum(total) > 50000
+
+-- 19 Calculate the total orders  amount for morning (6 AM to 12 PM), afternoon (12 PM to 6 PM), and evening (6 PM to 12 AM) periods using the time condition.
+
+with new_table
+as
+(select *,
+    case 
+      when extract(hour from time) between 6 and 12 then 'Morning'
+	  when extract (hour from time) >12 and extract(hour from time)<=18 then 'Afternoon'
+	  else 'Evening'
+	end as shift
+from walmart_sales
 )
-SELECT 
-    shift,
-    SUM(total) as total_sales,
-    COUNT(invoice_id) as total_orders
-FROM new_table
-WHERE branch <> 'A'
-GROUP BY shift
-HAVING COUNT(invoice_id) < 500
+select 
+      shift,
+	  count(invoice_id),
+	  sum(total) as total_orders_amount
+from new_table 
+group by 1
+
+
+
 
 
 
